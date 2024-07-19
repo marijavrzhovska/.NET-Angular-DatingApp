7ae9d0941c38ba5e0e0cc27f333784236a022130
marijavrzhovska <150076170+marijavrzhovska@users.noreply.github.com>
2024-07-19 19:48:40 +0200

    Created DbContext and new Api Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using API.Data;
+
+namespace API;
+[ApiController]
+[Route("api/[Controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase{
+    //endpoints
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users = context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id:int}")] // /api/users/id
+    public ActionResult<AppUser> GetUser(int id){
+        var user = context.Users.Find(id);
+        if(user == null) return NotFound();
+        return user;
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+//primary constructor
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
